91a31874831aa628761b60adfd9ae78b02895294
shyam-art <skmpakkam111@gmail.com>
2020-12-17 23:22:54 +0530

    tread-app-trial

---
+++ api/Car.ts
@@ -0,0 +1,12 @@
+export interface CarModel {
+    id: number;
+    make: string;
+    model: string;
+    year: number;
+    fuelType: string;
+    kilometers: number;
+    details: string;
+    price: number;
+    photoUrl: string;
+  }
+  
